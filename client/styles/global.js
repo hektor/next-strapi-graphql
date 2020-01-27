a8775103c2d3de8751eee77a11f1ee3d1a538cbd
h <hektor.misplon@gmail.com>
2020-01-27 16:32:50 +0100

    Add basic button styling

--- client/styles/global.js
+++ client/styles/global.js
@@ -13,9 +13,17 @@ export default css.global`
     display: flex;
     flex-direction: row;
     align-items: center;
+    padding: 0.5rem 1rem 0.5rem 0.5rem;
+    border: none;
+  }
+
+  .-outline {
+    background: transparent;
+    border: 1px solid;
   }
 
   button > svg {
+    height: 1rem;
     margin-right: 0.5rem;
   }
 `;
