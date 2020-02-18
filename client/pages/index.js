e9947c7c2c089544c814e6c0c38eb9ae86338920
Hektor Misplon <hektor.misplon@gmail.com>
2020-02-18 20:49:30 +0000

    Add news carousel

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,44 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import Carousel from 'nuka-carousel'
 
 export default () => (
 	<Layout>
-		<h1>Welkom bij Ouderenraad Gent</h1>
-		<h2>Nieuws</h2>
+		<div className="page-container">
+			<h1>Welkom bij Ouderenraad Gent</h1>
+			<div className="news">
+				<h2>Nieuws</h2>
+				<Carousel autoplay={true} autoplayInterval={5000}> 
+					<div className="news-card">
+						<h3>News 01</h3>
+					</div>
+					<div className="news-card">
+						<h3>News 02</h3>
+					</div>
+				</Carousel>
+			</div>
+			<div className="calendar">
+				<h2>Kalender</h2>
+			</div>
+			<style jsx>{`
+
+      .page-container {
+        display: flex;
+        flex: 1;
+      }
+
+      div {
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+      }
+
+      .news-card {
+        padding: 8rem 2rem;
+        background: var(--color-primary);
+        border-radius: var(--border-radius);
+      }
+    `}</style>
+		</div>
 	</Layout>
 )
