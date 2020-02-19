ad85dbc851fd752ff37f7106e0340cd19c97dda7
Hektor Misplon <hektor.misplon@gmail.com>
2020-02-19 09:23:58 +0000

    Add news slider

--- client/pages/index.js
+++ client/pages/index.js
@@ -4,41 +4,48 @@ import Carousel from 'nuka-carousel'
 
 export default () => (
 	<Layout>
-		<div className="page-container">
-			<h1>Welkom bij Ouderenraad Gent</h1>
 			<div className="news">
 				<h2>Nieuws</h2>
-				<Carousel autoplay={true} autoplayInterval={5000}> 
-					<div className="news-card">
-						<h3>News 01</h3>
-					</div>
-					<div className="news-card">
-						<h3>News 02</h3>
-					</div>
-				</Carousel>
+				<div className="slider">
+					<Carousel autoplay={true} autoplayInterval={5000}> 
+						<div className="news-card">
+              <h3>News 01</h3>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit, harum possimus similique voluptatem porro quidem corrupti perferendis dolores iusto voluptas mollitia odio unde, ipsum quaerat suscipit modi debitis, magnam deleniti.Recusandae accusamus neque quae libero consequuntur cupiditate vero, totam odio ipsum tempora dolorum nihil necessitatibus delectus distinctio ducimus consectetur, iste, cumque eos fugiat voluptate excepturi exercitationem inventore? Saepe, corrupti odio!</p>
+						</div>
+						<div className="news-card">
+							<h3>News 02</h3>
+						</div>
+					</Carousel>
+				</div>
 			</div>
 			<div className="calendar">
 				<h2>Kalender</h2>
 			</div>
 			<style jsx>{`
 
-      .page-container {
+      .page-container > * {
         display: flex;
-        flex: 1;
       }
 
-      div {
-        display: flex;
-        flex-direction: column;
-        flex: 1;
+      .news {
+        flex: .25;
+        margin-right: 4rem;
       }
 
-      .news-card {
-        padding: 8rem 2rem;
+      .slider {
+        flex: 0.5;
+        display: flex;
         background: var(--color-primary);
         border-radius: var(--border-radius);
       }
+
+      .news-card {
+        display: flex;
+        flex-direction: column;
+        padding: 2vw 2vw;
+        color: #fff;
+      }
     `}</style>
-		</div>
 	</Layout>
 )
+
