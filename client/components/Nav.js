126cbbccc0ce499b33c8bde0e96f347feb90f34c
Hektor Misplon <hektor.misplon@gmail.com>
2020-02-19 15:05:47 +0000

    Fix mobile menu scroll

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Link from './Link'
 import Search from '../components/Search'
+import {Menu, X} from 'react-feather'
 
 export default () => {
 	const [toggle, setToggle] = useState(false)
@@ -10,70 +11,93 @@ export default () => {
 				className="toggle"
 				onClick={() => setToggle(!toggle)}
 			>
-				{toggle ? 'close' : 'menu'}
+				<div className="icon">
+					{toggle ? <X /> : <Menu />} 
+				</div>
 			</button>
 			<nav>
 				<Search />
-				<Link href="/">
-					<a>Ouderenraad</a>
-				</Link>
-				<Link href="">
-					<a>Communicatie & info</a>
-				</Link>
-				<Link href="">
-					<a>Gezondheid & zorg</a>
-				</Link>
-				<Link href="">
-					<a>Infrastructuur</a>
-				</Link>
-				<Link href="">
-					<a>Mobiliteit & transport</a>
-				</Link>
-				<Link href="">
-					<a>Respect & inclusie</a>
-				</Link>
-				<Link href="">
-					<a>Wonen</a>
-				</Link>
-				<Link href="">
-					<a>Participatie</a>
-				</Link>
-				<Link href="/documents, /document">
-					<a>Documenten</a>
-				</Link>
+				<div className="nav-group">
+					<Link href="/">
+						<a>Ouderenraad</a>
+					</Link>
+					<Link href="/communicatie-info">
+						<a>Communicatie & info</a>
+					</Link>
+					<Link href="">
+						<a>Gezondheid & zorg</a>
+					</Link>
+					<Link href="">
+						<a>Infrastructuur</a>
+					</Link>
+					<Link href="">
+						<a>Mobiliteit & transport</a>
+					</Link>
+					<Link href="">
+						<a>Respect & inclusie</a>
+					</Link>
+					<Link href="">
+						<a>Wonen</a>
+					</Link>
+					<Link href="">
+						<a>Participatie</a>
+					</Link>
+					<Link href="/documents, /document">
+						<a>Documenten</a>
+					</Link>
+				</div>
 			</nav>
 			<style jsx>{`
+      
 				nav {
+          display: flex;
 					position: absolute;
 					bottom: 0;
 					width: 100vw;
 					height: 50vh;
-					display: ${toggle ? 'flex' : 'none'};
+					transform: translateY(${toggle ? '0' : '50vh'});
 					flex-direction: column;
-					padding-left: 0.5rem;
-					overflow-y: auto;
+					padding-left: 0.8rem;
 					background: #fff;
+          transition:  0.15s cubic-bezier(0.4, 0.14, 0.3, 1); 
 				}
+
+        .nav-group {
+          overflow-y: auto;
+        }
 
 				a {
 					flex: 1;
 					text-decoration: none;
 					display: flex;
 					align-items: center;
+          padding: 1.6rem;
+          border-bottom: 1px solid #eee;
 				}
 
 				.toggle {
-					position: absolute;
-					bottom: 0;
+          display: block;
+          position: absolute;
+          bottom: ${!toggle ? '0' : '46vh'};
 					right: 0;
-					z-index: 1;
+          width: 4rem;
+          height: 4rem;
+          padding: .8rem;
+          padding-bottom: .4rem;
+          margin: 2vw;
+          border-radius: .8rem;
+          outline: none;
+          z-index: 1;
+          transition:  0.15s cubic-bezier(0.4, 0.14, 0.3, 1); 
 				}
 
 				@media (min-width: 768px) {
 					nav {
 						position: static;
 						width: auto;
-						height: auto;
+            flex:1;
+            transform: none;
+            height: 100vh;
 						display: flex;
 					}
 
