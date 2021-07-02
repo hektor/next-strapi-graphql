bb2c0a4eda8946ae4aee7c005eae673472730856
Hektor Misplon <hektor.misplon@pm.me>
2021-07-02 20:08:23 +0200

    Use implicit return

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,20 +2,18 @@ import Head from 'next/head'
 import { ApolloProvider } from '@apollo/react-hooks'
 import withData from '../apollo/apollo'
 
-const App = ({ Component, pageProps, apollo }) => {
-	return (
-		<ApolloProvider client={apollo}>
-      <Head>
-        <title></title>
-				<link
-					rel="stylesheet"
-					href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.css"
-				/>
-				<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-			</Head>
-			<Component {...pageProps}></Component>
-		</ApolloProvider>
-	)
-}
+const App = ({ Component, pageProps, apollo }) => (
+  <ApolloProvider client={apollo}>
+    <Head>
+      <title></title>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.css"
+      />
+      <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+    <Component {...pageProps}></Component>
+  </ApolloProvider>
+)
 
 export default withData(App)
